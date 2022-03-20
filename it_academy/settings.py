1f673590b8255bdccb8c09a43c1ca14b2a1564aa
Baha <toktobekov1995@gmail.com>
2022-03-20 20:52:07 +0600

    update fixed allowed hosts

--- it_academy/settings.py
+++ it_academy/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6+$^!&^sc33^k(-%+%n+=bq0(8-3li1hgi-w-o+qmxkt78wx$6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['itacademy0312.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
