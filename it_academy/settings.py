7cc73c9c4b0669e64d8cb5799add3586dd7811b3
Baha <toktobekov1995@gmail.com>
2022-03-20 20:57:34 +0600

    bag fixed settings.py update PROJECT_ROOT

--- it_academy/settings.py
+++ it_academy/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
